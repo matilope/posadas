da1dee1f4f9556cce474b71632a800ef3e4190f4
Matias Lopez <mati021796@gmail.com>
2022-07-04 01:40:11 -0300

    possible final push

--- post.php
+++ post.php
@@ -4,41 +4,38 @@ $nombre = $_POST["nombre"];
 $email = $_POST["correo"];
 $fechaIda = $_POST["fechaIda"];
 $fechaVuelta = $_POST["fechaVuelta"];
-$imagen = $_FILES['imagen']['name']; 
+$imagen = $_FILES['imagen']['tmp_name']; 
 $consulta = $_POST["mensaje"];
 $actividad=$_POST['actividad'];
 
-move_uploaded_file($_FILES['imagen']['tmp_name'], "https://posadasciudad.herokuapp.com/img/formulario/".$imagen);
+// move_uploaded_file($_FILES['imagen']['tmp_name'], "img/formulario/".$imagen);
 
 $destinatario = "mati02171996@gmail.com";
 $asunto = "Reserva turismo posadas";
-$mensaje="<table style='margin:0 auto; background-color:rgb(0,128,0);color:white; padding:20px; border-radius:20px; box-shadow:1px 1px 10px rgb(0,128,0);'>
-<h1 style='text-align:center; color:rgb(0,128,0); text-transform:capitalize;'>¡Hola $nombre! Hemos recibido tu consulta correctamente</h1>
+$mensaje="<table style='margin: 0 auto; width: 360px; background-color: rgb(237, 237, 237); color: #000000; padding: 20px; border-radius: 20px; border: 1px solid #007e0fdb;'>
+<caption style='text-align:center; color:rgb(0,128,0); text-transform:capitalize; font-size:20px; margin-bottom:10px;'>¡Has recibido una reserva de $nombre!</caption>
+<tbody>
 <tr>
-    <th>Correo</th>
-    <th>Actividades</th>
-    <th>Fecha ida</th>
-    <th>Fecha vuelta</th>
-    <th>Consulta</th>
-    <th>Imagen</th>
+    <th style='vertical-align: top; text-align: left;'>Correo</th>
+    <td>$email</td>
 </tr>
-<tr>
-    <td>$email</td>";
+<tr><th style='vertical-align: top; text-align: left;'>Actividades</th><td>";
+
 
 for ($x = 1; $x < count($actividad); $x++) {
 $mensaje.= $actividad[$x-1].", ";
 	if($x+1==count($actividad)){
-	$mensaje.= "<td>".$actividad[$x].".</td>";
+	$mensaje.= $actividad[$x].".";
  };
 };
 
-$mensaje.= "<td>".$fechaIda."</td><td>".$fechaVuelta."</td>";
+$mensaje.= "</td></tr><tr><th style='vertical-align: top; text-align: left;'>Fecha ida</th><td>".$fechaIda."</td></tr><tr><th style='vertical-align: top; text-align: left;'>Fecha vuelta</th><td>".$fechaVuelta."</td></tr>";
 
 if(strlen($consulta)>=1){
-	$mensaje.= "<td>".$consulta."</td>";
+	$mensaje.= "<tr><th style='vertical-align: top; text-align: left;'>Consulta</th><td>".$consulta."</td></tr>";
 }
 
-$mensaje.= "<td><img src='https://posadasciudad.herokuapp.com/img/formulario/".$imagen."'/></td></tr></table>";
+$mensaje .= "</tbody></table>";
 
 $headers = "Mime-version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n";
@@ -56,10 +53,11 @@ $mg = Mailgun::create('cfde54b68d339230200befc7ceccef79-77985560-ccbed702'); // 
 // Now, compose and send your message.
 // $mg->messages()->send($domain, $params);
 $mg->messages()->send('sandbox53e4bca9c3c34c3eabe4ccf736994525.mailgun.org', [
-  'from'    =>  "$email",
-  'to'      => "$destinatario",
-  'subject' => "$asunto",
-  'html'    => "$mensaje"
+  'from'    =>  $email,
+  'to'      => $destinatario,
+  'subject' => $asunto,
+  'html'    => $mensaje,
+  'attachment' => [['filePath'=>$imagen, 'filename'=>'imagen_dni.jpg']]
 ]);
 
 // mail($destinatario, $asunto, $mensaje, $headers);
